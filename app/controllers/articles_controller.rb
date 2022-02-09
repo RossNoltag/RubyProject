6e781e6995b247ab5e81904b8dee103e952953e7
RossNoltag <irlnoltag@gmail.com>
2022-02-09 14:33:13 +0000

    CRUD in place, to fix missing method error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,36 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
   def create
+    @article= Article.new(title:"...",body:"...")
+    if @article.save
+      redirect_to @article
+    else
+      reder :new,status: uprocessable_entity
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])#used for when building the form
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else 
+      render :edit, status: uprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 
 end
--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,16 +1,52 @@
 class TestController < ApplicationController
   
   def index
-    @testlist=Test.all
+    @testlist=Test.all 
   end
 
   def show
-    @testShow = Test.find(params[:id])
+    @test = Test.find(params[:id])
+    @id = params[:id]
   end
 
-  def delete
-    @testDelete = Test.Destroy()
+  def new
+    @newTest = Test.new
   end
 
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new, status: :uprocessable_entity
+    end
+  end
+
+  def edit
+    @test = Test.find(params[:id])#used for when building the form
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to @test
+    else 
+      render :edit, status: uprocessable_entity
+    end
+  end
+
+  def destroy
+    @test = Article.find(params[:id])
+    @test.destroy
+    
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def test_params
+    params.require(:test).permit(:title, :body)
+  end
   
 end
